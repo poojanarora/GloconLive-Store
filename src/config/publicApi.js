b6929db191cc9f41833e0cf4543b357431202ecd
Karan Jamadar <kjamadar@novatechset.com>
2022-12-02 12:54:44 +0000

    Updated api URL to https

--- src/config/publicApi.js
+++ src/config/publicApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://glocon-live.katdev.com/api";
+const API_URL = "https://glocon-live.katdev.com/api";
 
 const axiosPublic = axios.create({
     baseURL: API_URL,
